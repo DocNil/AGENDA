44a325c83fbaf00e10019b41261a361e89c96d19
DocNil <nilsonfabris@yahoo.com>
2025-03-12 10:31:39 -0300

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE agenda;
+
+USE agenda;
+
+CREATE TABLE contacts (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    phone VARCHAR(30) NOT NULL
+);
